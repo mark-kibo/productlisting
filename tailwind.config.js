1704be4fd60c46286b611d3361498368b02354eb
mark kibocha <kibochamark@gmail.com>
2024-03-07 18:21:18 +0300

    Creation of Register Page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#F49B24",
+        textprimary:"#A58A5E",
+        textbackground:"#F3E5E2"
+      }
+    },
+  },
+  plugins: [],
+}
+
